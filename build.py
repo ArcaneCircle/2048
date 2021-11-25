e0e71801e0cdf0a90cd52063133b90690ce2c5fb
adbenitez <asieldbenitez@gmail.com>
2021-11-25 18:44:01 -0500

    improve build.py, make minification optional

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import os
 
 import htmlmin
@@ -6,7 +7,23 @@ import lesscpy
 from jinja2 import Environment, PackageLoader, select_autoescape
 from jsmin import jsmin
 
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="App Builder",
+    )
+    parser.add_argument(
+        "-m",
+        "--minify",
+        action="store_true",
+        help="shrink the app's source code removing unnecesary spaces, etc.",
+    )
+
+    return parser
+
+
 if __name__ == "__main__":
+    args = get_parser().parse_args()
     env = Environment(
         loader=PackageLoader("build", "."),
         autoescape=select_autoescape(["html", "xml"]),
@@ -14,16 +31,24 @@ if __name__ == "__main__":
     scripts = []
     for filename in sorted(os.listdir("js")):
         with open(os.path.join("js", filename)) as file:
-            scripts.append(jsmin(file.read()).replace("\n", ";"))
-    min_script = ";".join(scripts)
+            if args.minify:
+                scripts.append(jsmin(file.read()).replace("\n", ";"))
+            else:
+                scripts.append(file.read())
+    script = ";".join(scripts)
 
     with open(os.path.join("style", "main.css")) as file:
-        min_css = lesscpy.compile(file, minify=True, xminify=True)
+        if args.minify:
+            css = lesscpy.compile(file, minify=True, xminify=True)
+        else:
+            css = file.read()
 
     template = env.get_template("index.html.j2")
-    min_html = htmlmin.minify(template.render(css=min_css, script=min_script))
+    html = template.render(css=css, script=script)
+    if args.minify:
+        html = htmlmin.minify(html)
 
     if not os.path.exists("dist"):
         os.makedirs("dist")
     with open(os.path.join("dist", "2048.html.w30"), "w") as file:
-        file.write(min_html)
+        file.write(html)
