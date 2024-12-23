60ed8aa4e0b265620512ec3c84edc787b7583c53
adbenitez <asieldbenitez@gmail.com>
2024-12-23 11:37:11 +0100

    apply prettier

--- src/application.js
+++ src/application.js
@@ -7,17 +7,22 @@ import { KeyboardInputManager } from "./keyboard_input_manager.js";
 import { HTMLActuator } from "./html_actuator.js";
 
 const scoreboard = document.getElementById("scoreboard");
-window.highscores.init({
-  onHighscoresChanged: () => {
-    scoreboard.innerHTML = window.highscores.renderScoreboard().innerHTML;
-  },
-}).then(() => {
-  // Wait till the browser is ready to render the game (avoids glitches)
-  window.requestAnimationFrame(() => {
-    const gameManager = new GameManager(4, KeyboardInputManager, HTMLActuator);
-    document.addEventListener("visibilitychange", () => {
-      window.highscores.setScore(gameManager.score);
+window.highscores
+  .init({
+    onHighscoresChanged: () => {
+      scoreboard.innerHTML = window.highscores.renderScoreboard().innerHTML;
+    },
+  })
+  .then(() => {
+    // Wait till the browser is ready to render the game (avoids glitches)
+    window.requestAnimationFrame(() => {
+      const gameManager = new GameManager(
+        4,
+        KeyboardInputManager,
+        HTMLActuator,
+      );
+      document.addEventListener("visibilitychange", () => {
+        window.highscores.setScore(gameManager.score);
+      });
     });
   });
-});
-
